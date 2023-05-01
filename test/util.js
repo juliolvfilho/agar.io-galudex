f8dd8aad901b8ca7c115ec6fdeb7afb75f334938
juliolvfilho <juliolvfilho@gmail.com>
2023-05-01 11:16:14 -0300

    style: format test/util.js

--- test/util.js
+++ test/util.js
@@ -1,7 +1,7 @@
 /*jshint expr:true */
 
 var expect = require('chai').expect,
-    util   = require('../src/server/lib/util');
+  util = require('../src/server/lib/util');
 
 
 /**
@@ -23,8 +23,8 @@ describe('util.js', function () {
 
     it('should convert masses to a circle radius', function () {
       var r1 = util.massToRadius(4),
-          r2 = util.massToRadius(16),
-          r3 = util.massToRadius(1);
+        r2 = util.massToRadius(16),
+        r3 = util.massToRadius(1);
 
       expect(r1).to.equal(16);
       expect(r2).to.equal(28);
@@ -42,10 +42,10 @@ describe('util.js', function () {
 
     it('should allow ascii character nicknames', function () {
       var n1 = util.validNick('Walter_White'),
-          n2 = util.validNick('Jesse_Pinkman'),
-          n3 = util.validNick('hank'),
-          n4 = util.validNick('marie_schrader12'),
-          n5 = util.validNick('p');
+        n2 = util.validNick('Jesse_Pinkman'),
+        n3 = util.validNick('hank'),
+        n4 = util.validNick('marie_schrader12'),
+        n5 = util.validNick('p');
 
       expect(n1).to.be.true;
       expect(n2).to.be.true;
@@ -61,8 +61,8 @@ describe('util.js', function () {
     });
 
     it('should disallow spaces in nicknames', function () {
-        var n1 = util.validNick('Walter White');
-        expect(n1).to.be.false;
+      var n1 = util.validNick('Walter White');
+      expect(n1).to.be.false;
     });
   });
 
@@ -70,11 +70,11 @@ describe('util.js', function () {
 
     it('should compute the log_{base} of a number', function () {
       var base10 = util.log(1, 10),
-          base2  = util.log(1, 2);
+        base2 = util.log(1, 2);
 
       var identity = util.log(10, 10);
 
-      var logNineThree = Math.round(util.log(9,3) * 1e5) / 1e5; // Tolerate rounding errors
+      var logNineThree = Math.round(util.log(9, 3) * 1e5) / 1e5; // Tolerate rounding errors
 
       //  log(1) should equal 0, no matter the base
       expect(base10).to.eql(base2);
@@ -91,17 +91,17 @@ describe('util.js', function () {
   describe('#getDistance', function () {
 
     // helper class
-    function Point(x,y,r) {
+    function Point(x, y, r) {
       return {
-        x      : x,
-        y      : y,
-        radius : r
+        x: x,
+        y: y,
+        radius: r
       };
     }
 
     var p1 = Point(-100, 20, 1),
-        p2 = Point(0, 40, 5),
-        p3 = Point(0,0,100);
+      p2 = Point(0, 40, 5),
+      p3 = Point(0, 0, 100);
 
     it('should return a positive number', function () {
       var d = util.getDistance(p1, p2);
